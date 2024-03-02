f0fd9d836604f97fa2ba0fc5fecd642f2215f509
15084 <15084@LAPTOP-O0VP2NQS>
2024-03-02 18:41:06 +0800

    添加项目文件。

---
+++ Class1.cs
@@ -0,0 +1,46 @@
+﻿using AimRobot.Api;
+using AimRobot.Api.plugin;
+using ar_example_plugin;
+
+namespace robot_kpm
+{
+    public class Class1 : PluginBase
+    {
+
+        public override string GetAuthor()
+        {
+            return "H4rry217";
+        }
+
+        public override string GetDescription()
+        {
+            return "这是一个示范插件，用于演示如何编写ARL插件";
+        }
+
+        public override string GetPluginName()
+        {
+            return "robot_kpm";
+        }
+
+        public override Version GetVersion()
+        {
+            return new Version(1, 0, 0);
+        }
+
+        public override void OnDisable()
+        {
+
+        }
+
+        public override void OnEnable()
+        {
+
+        }
+
+        public override void OnLoad()
+        {
+            Robot.GetInstance().GetPluginManager().RegisterListener(this, new LimitPlayerStatsPlugin());
+        }
+
+    }
+}
---
+++ LimitPlayerStatsPlugin.cs
@@ -0,0 +1,25 @@
+﻿using AimRobot.Api;
+using AimRobot.Api.events;
+using AimRobot.Api.game;
+namespace ar_example_plugin
+{
+    public class LimitPlayerStatsPlugin : IEventListener
+    {
+        private const double MAX_KPM = 1.0;  // 允许的每分钟最大击杀数
+        private const int MAX_RANK = 10;     // 所需的最GAO等级
+        private const int MAX_KILLS = 100;   // 允许的最大击杀数
+        [AimRobot.Api.events.EventHandler]
+        public void OnPlayerStatsReceived(PlayerStatInfo playerStats)
+        {
+
+            // 检查玩家状态是否超出限制
+            if (playerStats.killsPerMinute > MAX_KPM || playerStats.rank < MAX_RANK || playerStats.kills > MAX_KILLS)
+            {
+                string message = "你已经超出kpm或击杀限制";
+                Robot.GetInstance().SendChat(message);
+                Robot.GetInstance().BanPlayer(playerStats.id);
+
+            }
+        }
+    }
+}
